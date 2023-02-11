93c59ef174c1996cc2e231fb7e8a582e9dd6243f
Falcion <falcionml@gmail.com>
2023-02-11 12:30:06 +0300

    Deploy of DEMO (more in CHANGELOG)

---
+++ Kern.cs
@@ -0,0 +1,181 @@
+﻿using Briefsmaschine;
+using System.Diagnostics;
+using System.Text;
+using static Briefsmaschine.Objects;
+
+namespace Briefmaschine
+{
+    public static class Kern
+    {
+        public static void Info(string message, string code_pos = "DEFAULT", bool is_io = false)
+        {
+            var stackframe = new StackFrame(1);
+
+            string? methodName = stackframe.GetMethod()?.Name;
+
+            string datetime = DateTime.Now.ToString("dd.MM.yyyy HH:mm:ss");
+
+            string logEntry = $"[INFO/{code_pos.ToUpper()}] < STACKFRAME: {(methodName == null ? "UNDEFINED" : methodName.ToUpper())} > | {datetime} - {message}";
+
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.BackgroundColor = ConsoleColor.Black;
+
+            Console.Out.WriteLine(logEntry);
+
+            Console.ResetColor();
+
+            if(is_io)
+            {
+                Profiler.EnsureLogs();
+                string? path = Environment.GetEnvironmentVariable("LOGS_PATH");
+                Profiler.UpdateLogs(logEntry, path);
+            }
+        }
+
+        public static void Warn(string message, string code_pos = "DEFAULT", bool is_io = false)
+        {
+            var stackframe = new StackFrame(1);
+
+            string? methodName = stackframe.GetMethod()?.Name;
+
+            string datetime = DateTime.Now.ToString("dd.MM.yyyy HH:mm:ss");
+
+            string logEntry = $"[WARN/{code_pos.ToUpper()}] < STACKFRAME: {(methodName == null ? "UNDEFINED" : methodName.ToUpper())} > | {datetime} - {message}";
+
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.BackgroundColor = ConsoleColor.Black;
+
+            Console.Out.WriteLine(logEntry);
+
+            Console.ResetColor();
+
+            if (is_io)
+            {
+                Profiler.EnsureLogs();
+                string? path = Environment.GetEnvironmentVariable("LOGS_PATH");
+                Profiler.UpdateLogs(logEntry, path);
+            }
+        }
+
+        public static void Error(Exception exception, string code_pos = "DEFAULT")
+        {
+            string datetime = DateTime.Now.ToString("dd.MM.yyyy HH:mm:ss");
+
+            string logEntry = $"[ERROR/{code_pos.ToUpper()}] < TRACE: {exception.StackTrace} & TARGET: {(exception.TargetSite == null ? "UNDEFINED" : exception.TargetSite.Name)}> " +
+                $"{datetime} - {exception.Message}";
+
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.BackgroundColor = ConsoleColor.Black;
+
+            Console.Out.WriteLine(logEntry);
+
+            Console.ResetColor();
+
+            Profiler.EnsureLogs();
+            string? path = Environment.GetEnvironmentVariable("LOGS_PATH");
+            Profiler.UpdateLogs(logEntry, path);
+        }
+
+        public static void Error(string message, string code_pos = "DEFAULT", bool is_io = false)
+        {
+            var stackframe = new StackFrame(1);
+
+            string? methodName = stackframe.GetMethod()?.Name;
+
+            string datetime = DateTime.Now.ToString("dd.MM.yyyy HH:mm:ss");
+
+            string logEntry = $"[ERROR/{code_pos.ToUpper()}] < STACKFRAME: {(methodName == null ? "UNDEFINED" : methodName.ToUpper())} > | {datetime} - {message}";
+
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.BackgroundColor = ConsoleColor.Black;
+
+            Console.Out.WriteLine(logEntry);
+
+            Console.ResetColor();
+
+            if (is_io)
+            {
+                Profiler.EnsureLogs();
+                string? path = Environment.GetEnvironmentVariable("LOGS_PATH");
+                Profiler.UpdateLogs(logEntry, path);
+            }
+        }
+
+        public static void Success(string message, string code_pos = "DEFAULT", bool is_io = false)
+        {
+            var stackframe = new StackFrame(1);
+
+            string? methodName = stackframe.GetMethod()?.Name;
+
+            string datetime = DateTime.Now.ToString("dd.MM.yyyy HH:mm:ss");
+
+            string logEntry = $"[SUCCESS/{code_pos.ToUpper()}] < STACKFRAME: {(methodName == null ? "UNDEFINED" : methodName.ToUpper())} > | {datetime} - {message}";
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.BackgroundColor = ConsoleColor.Black;
+
+            Console.Out.WriteLine(logEntry);
+
+            Console.ResetColor();
+
+            if (is_io)
+            {
+                Profiler.EnsureLogs();
+                string? path = Environment.GetEnvironmentVariable("LOGS_PATH");
+                Profiler.UpdateLogs(logEntry, path);
+            }
+        }
+
+        public static class Customs
+        {
+            public static void Log(string message, string type = "CUSTOM", string code_pos = "DEFAULT",
+                ColorsPair? colors = null,
+                bool is_io = false, string? folder = null, string? file = null)
+            {
+                var stackframe = new StackFrame(1);
+
+                string? methodName = stackframe.GetMethod()?.Name;
+
+                string logEntry = $"[{type.ToUpper()}/{code_pos.ToUpper()}] < STACKFRAME: {(methodName == null ? "UNDEFINED" : methodName.ToUpper())} > | {DateTime.Now:dd.MM.yyyy HH:mm:ss} - {message}";
+
+                if (colors == null)
+                    colors = new ColorsPair();
+
+                Console.BackgroundColor = colors.BackgroundColor;
+                Console.ForegroundColor = colors.ForegroundColor;
+
+                Console.Out.WriteLine(logEntry);
+
+                Console.ResetColor();
+
+                if (is_io)
+                {
+                    if (folder == null || folder == "" || folder.Length < 1)
+                        folder = "logs";
+
+                    if (file == null || file == "" || file.Length < 1)
+                        file = $"session-{DateTime.Now:dd-MM-yyyy}.logs";
+
+                    DirectoryInfo dir = new(folder);
+
+                    if (dir.Exists)
+                        dir.Attributes = FileAttributes.Directory | FileAttributes.Hidden;
+                    else
+                    {
+                        dir.Create();
+                        dir.Attributes = FileAttributes.Directory | FileAttributes.Hidden;
+                    }
+
+                    if (!File.Exists(Path.Combine(folder, file)))
+                        using (FileStream stream = File.Create(Path.Combine(folder, file)))
+                        {
+                            byte[] msg = Encoding.UTF8.GetBytes(logEntry);
+
+                            stream.Write(msg, 0, msg.Length);
+                            stream.Close();
+                        }
+                }
+            }
+        }
+    }
+}
---
+++ Objects.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Briefsmaschine
+{
+    public static class Objects
+    {
+        public class ColorsPair
+        {
+            public readonly ConsoleColor ForegroundColor;
+            public readonly ConsoleColor BackgroundColor;
+
+            public ColorsPair(ConsoleColor foregroundColor, ConsoleColor backgroundColor)
+            {
+                ForegroundColor = foregroundColor;
+                BackgroundColor = backgroundColor;
+            }
+
+            public ColorsPair(ConsoleColor foregroundColor)
+            {
+                ForegroundColor = foregroundColor;
+                BackgroundColor = ConsoleColor.Black;
+            }
+
+            public ColorsPair()
+            {
+                ForegroundColor = ConsoleColor.Gray;
+                BackgroundColor = ConsoleColor.Black;
+            }
+        }
+    }
+}
